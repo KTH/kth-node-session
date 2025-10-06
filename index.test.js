61ed9c0622bfb74f93d133d3ffd8a432bb33dd0c
Niklas Sandström <mr.n.sandstrom@gmail.com>
2025-10-06 12:56:05 +0200

    update to latest "connect-redis" and replace "redis" with "kth-node-redis"

--- index.test.js
+++ index.test.js
@@ -1,7 +1,10 @@
-jest.mock('redis', () => ({
-  createClient: jest.fn().mockImplementation(() => ({
-    on: jest.fn(),
-  })),
+const mockRedisClient = {
+  connect: jest.fn(),
+}
+mockRedisClient.connect.mockResolvedValue(mockRedisClient)
+
+jest.mock('kth-node-redis', () => ({
+  createClient: jest.fn().mockImplementation(() => mockRedisClient),
 }))
 
 describe(`Session`, () => {
@@ -10,13 +13,15 @@ describe(`Session`, () => {
   let createSession
 
   beforeEach(() => {
+    mockRedisClient.connect.mockResolvedValue(mockRedisClient)
+
     jest.resetModules()
     createSession = require('./index')
   })
 
   afterEach(() => {})
 
-  test('should not allow a session without secret', () => {
+  test('should not allow a session without a redis key', () => {
     try {
       const options = {
         key: 'MY_REDIS',
@@ -27,7 +32,7 @@ describe(`Session`, () => {
       expect(err).not.toBeUndefined()
     }
   })
-  test('should not allow a session without a redis key', () => {
+  test('should not allow a session without secret', () => {
     try {
       const options = {
         sessionOptions: {
@@ -52,5 +57,36 @@ describe(`Session`, () => {
     const session = createSession(options)
     expect(session).not.toBeUndefined()
     expect(session.constructor.name).toBe('Function')
+  })
+  test('should exit application if redis fails to connect', async () => {
+    // This test is a bit messy. If redisClient fails to connect, the application exits, but that happens AFTER createSession has returned
+    const mockExit = jest.spyOn(process, 'exit').mockImplementation(number => {
+      console.log('Mock process.exit with code', number)
+    })
+
+    const options = {
+      key: 'MY_REDIS',
+      sessionOptions: {
+        secret: '1234',
+      },
+      useRedis: true,
+    }
+
+    await new Promise((resolve, reject) => {
+      mockRedisClient.connect.mockRejectedValue('fail')
+      createSession(options)
+
+      setTimeout(() => {
+        try {
+          expect(mockExit).toHaveBeenCalledWith(1)
+          mockExit.mockRestore()
+          resolve()
+        } catch (e) {
+          console.log(e)
+          mockExit.mockRestore()
+          reject('Test failed')
+        }
+      }, 10)
+    })
   })
 })
